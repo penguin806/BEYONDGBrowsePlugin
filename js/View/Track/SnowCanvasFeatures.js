ff6e75f9147afed1fb1696e64a64f7ef6d799abd
Snow <polarpenguin@foxmail.com>
2019-06-09 03:18:04 +0800

    Updated: SnowCanvasFeatures.js

--- js/View/Track/SnowCanvasFeatures.js
+++ js/View/Track/SnowCanvasFeatures.js
@@ -4,13 +4,17 @@ define(
         'dojo/_base/declare',
         'dojo/request',
         'dojo/dom-construct',
-        'JBrowse/View/Track/CanvasFeatures'
+        'dojo/Deferred',
+        'JBrowse/View/Track/CanvasFeatures',
+        'JBrowse/Util'
     ],
     function (
         declare,
         request,
         domConstruct,
-        CanvasFeatures
+        dojoDeferred,
+        CanvasFeatures,
+        Util
     ) {
 
         return declare(
@@ -18,93 +22,152 @@ define(
                 CanvasFeatures
             ],
             {
-                // construct: function (arg) {
-                //
-                // }
-                queryFeatures: function(refName, startPos, endPos){
-                    request(
-                        'http://172.25.176.241:12345/' + refName + '/' +
-                        startPos + '..' + endPos + '/uniprot_id',
+                construct: function (arg) {
+                    this._codonTable = this.defaultCodonTable();
+                },
+
+                _translateSequenceToProtein: function(sequence, isReverse){
+                    let threeTranslatedSeqs = [];
+
+                    for(let offset=0; offset<3; offset++)
+                    {
+                        let slicedSequence = sequence.slice(offset,sequence.length-offset);
+                        let translatedSeq = "";
+                        for(let i=0; i<slicedSequence.length; i+=3)
                         {
-                            method: 'get',
+                            let theCodon = slicedSequence.slice(i, i+3);
+                            let aminoAcid = this._codonTable[theCodon] || '#';
+                            translatedSeq += aminoAcid;
+                        }
+
+                        threeTranslatedSeqs[offset] = translatedSeq;
+                    }
+
+                    return threeTranslatedSeqs;
+                },
+
+                queryFeatures: function(refName, startPos, endPos){
+                    let requestPromise = request(
+                        'http://172.25.176.241:12345/' + refName + '/' +
+                        startPos + '..' + endPos + '/uniprot_id', {
+                            method: 'GET',
                             headers: {
-                                //'Origin': 'http://172.25.176.241:12345'
+                                'X-Requested-With': null
                                 //'User-Agent': 'SnowPlugin-FrontEnd'
                             },
                             handleAs: 'json'
                         }
-                        ).then(
-                            function (recordObject) {
-                                console.log(recordObject);
-                            }
-                    );
+                        );
+                    return requestPromise;
                 },
 
                 fillBlock: function(renderArgs) {
+                    let _this = this;
                     let blockIndex = renderArgs.blockIndex;
-                    let block = renderArgs.block;
-                    let blockWidthPx = block.domNode.offsetWidth
+                    let blockObject = renderArgs.block;
+                    let blockWidth = blockObject.domNode.offsetWidth;
                     let leftBase = renderArgs.leftBase;
                     let rightBase = renderArgs.rightBase;
-                    let scale = renderArgs.scale;
+                    let scaleLevel = renderArgs.scale;
 
-                    if( scale >5 )
+                    let getRefSeqDeferred = new dojoDeferred();
+                    let mapProteinSeqDeferred = new dojoDeferred();
+                    let drawResultsDeferred = new dojoDeferred();
+
+                    let dataObject = {
+                        translatedRefSeqs: null,
+                        proteinData: null
+                    };
+
+                    getRefSeqDeferred.then(
+                        function (refGenomeSeq) {
+                            // Execute when Retrieve reference sequence complete
+                            dataObject.translatedRefSeqs =
+                                this._translateSequenceToProtein(refGenomeSeq, false);
+
+                            let requestPromise = _this.queryFeatures(this.refSeq.name, leftBase, rightBase);
+                            return requestPromise;
+                        },
+                        function (errorReason) {
+                            console.error(errorReason);
+                        }
+                    ).then(
+                        function (recordObjectArray) {
+                            console.log(recordObjectArray);
+
+                            dataObject.proteinData = recordObjectArray;
+                            mapProteinSeqDeferred.resolve(dataObject);
+                        },
+                        function (reasonWhyRequestFail) {
+                            console.error(reasonWhyRequestFail);
+                        }
+                    );
+
+                    mapProteinSeqDeferred.then(
+                        function (dataObject) {
+
+                        }
+                    );
+
+                    drawResultsDeferred.then(
+                        function (obj) {
+                            let layout = this._getLayout( scaleLevel );
+                            let totalHeight = layout.getTotalHeight();
+                            domConstruct.empty( blockObject.domNode );
+                            let c = blockObject.featureCanvas =
+                                domConstruct.create(
+                                    'canvas',
+                                    {
+                                        height: totalHeight,
+                                        width:  blockObject.domNode.offsetWidth+1,
+                                        style: {
+                                            cursor: 'default',
+                                            height: totalHeight+'px',
+                                            position: 'absolute'
+                                        },
+                                        innerHTML: 'Your web browser cannot display this type of track.',
+                                        className: 'canvas-track'
+                                    },
+                                    blockObject.domNode
+                                );
+                            let ctx = c.getContext('2d');
+                            // scale the canvas to work well with the various device pixel ratios
+                            this._scaleCanvas(c);
+
+                            if (blockObject.maxHeightExceeded)
+                                this.markBlockHeightOverflow(blockObject);
+
+                            this.heightUpdate(totalHeight, blockIndex);
+
+                            // this.renderFeatures(args, fRects);
+                            // this.renderClickMap(args, fRects);
+                        }
+                    );
+
+                    if( scaleLevel > 5 )
                     {
                         this.store.getReferenceSequence(
                             {
-                                ref: this.refSeq.name,
+                                ref: this.translatedRefSeqs.name,
                                 start: leftBase,
                                 end: rightBase
                             },
-                            function( refDNASeq ) {
-                                console.log(refDNASeq);
+                            function( refGenomeSeq ) {
+                                getRefSeqDeferred.resolve(refGenomeSeq);
                             },
-                            function(error) {
-                                console.error(error);
+                            function(errorReason) {
+                                getRefSeqDeferred.reject(errorReason);
                             }
                         );
                     }
                     else
                     {
-                        console.log('Region too large: '+leftBase+'..'+rightBase);
+                        let errorMsg = 'Scale level is' + scaleLevel +
+                            ' (less than 5), range too large: ' + leftBase+'~'+rightBase;
+                        getRefSeqDeferred.reject(errorMsg);
                     }
 
-
-                    this.queryFeatures(this.refSeq.name, leftBase, rightBase);
-                    // Todo: Pass the following code as a callback function to <queryFeatures>
-
-                    const layout = this._getLayout( scale );
-                    const totalHeight = layout.getTotalHeight();
-                    domConstruct.empty( block.domNode );
-                    const c = block.featureCanvas =
-                        domConstruct.create(
-                            'canvas',
-                            {
-                                height: totalHeight,
-                                width:  block.domNode.offsetWidth+1,
-                                style: {
-                                    cursor: 'default',
-                                    height: totalHeight+'px',
-                                    position: 'absolute'
-                                },
-                                innerHTML: 'Your web browser cannot display this type of track.',
-                                className: 'canvas-track'
-                            },
-                            block.domNode
-                        );
-                    const ctx = c.getContext('2d');
-                    // scale the canvas to work well with the various device pixel ratios
-                    this._scaleCanvas(c);
-
-                    if (block.maxHeightExceeded)
-                        this.markBlockHeightOverflow(block);
-
-                    this.heightUpdate(totalHeight, blockIndex);
-
-                    // this.renderFeatures(args, fRects);
-                    // this.renderClickMap(args, fRects);
                 }
-
 
             }
         );
