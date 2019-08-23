1ac8170deac9561553bd4c6c1b491007498d174d
Snow <polarpenguin@foxmail.com>
2019-08-23 23:29:53 +0800

    Add MassTrackSetting Dialog

---
+++ js/View/Dialog/SnowMassTrackSettingDialog.js
@@ -0,0 +1,110 @@
+define(
+    [
+        'dojo/_base/declare',
+        'dojo/_base/lang',
+        'dojo/dom-class',
+        'dojo/dom-construct',
+        'dijit/focus',
+        'dijit/form/NumberSpinner',
+        'JBrowse/View/Dialog/WithActionBar',
+        'dojo/on',
+        'dijit/form/Button',
+    ],
+    function(
+        declare,
+        dojoLang,
+        domClass,
+        domConstruct,
+        focus,
+        dijitNumberSpinner,
+        ActionBarDialog,
+        on,
+        Button
+    ){
+        return declare(
+            [
+                ActionBarDialog
+            ],
+            {
+                constructor: function( args )
+                {
+                    this.title = 'Set mass spectra track number';
+
+                    this.browser = args.browser;
+                    this.setCallback    = args.setCallback || function() {};
+                    this.cancelCallback = args.cancelCallback || function() {};
+                },
+
+                _fillActionBar: function( actionBar )
+                {
+                    var _this = this;
+                    var cancelButton = new Button(
+                        {
+                            iconClass: 'dijitIconDelete', label: 'Cancel',
+                            onClick: function() {
+                                _this.cancelCallback && _this.cancelCallback();
+                                _this.hide();
+                            }
+                        }
+                    );
+                    cancelButton.placeAt( actionBar );
+
+                    var findButton = new Button(
+                        {
+                            iconClass: 'dijitIconSave',
+                            label: 'Save',
+                            onClick:function() {
+                                _this.setCallback && _this.setCallback(
+                                    _this.getMassSpectraTrackNumber()
+                                );
+                                _this.hide();
+                            }
+                        }
+                    );
+                    findButton.placeAt( actionBar );
+                },
+
+                show: function( callback ) {
+                    var _this = this;
+                    var massTrackNumber = _this.browser.config.massSpectraTrackNum ? _this.browser.config.massSpectraTrackNum : 0;
+                    domClass.add( this.domNode, 'massTrackSettingDialog' );
+
+                    _this.massTrackNumberInput = new dijitNumberSpinner(
+                        {
+                            id: 'mass_track_number',
+                            value: massTrackNumber,
+                            smallDelta: 1,
+                            constraints: {
+                                min: 0,
+                                max: 100
+                            }
+                        }
+                    );
+
+                    _this.set(
+                        'content',
+                        [
+                            // domConstruct.create('label', { "for": 'mass_track_number', innerHTML: 'Number' } ),
+                            _this.massTrackNumberInput.domNode
+                        ]
+                    );
+
+                    _this.inherited(arguments);
+                },
+
+                getMassSpectraTrackNumber: function() {
+                    var massSpectraTrackNumber = this.massTrackNumberInput.getValue();
+                    return massSpectraTrackNumber;
+                },
+
+                hide: function() {
+                    this.inherited(arguments);
+                    window.setTimeout(
+                        dojoLang.hitch( this, 'destroyRecursive' ),
+                        500
+                    );
+                }
+            }
+        );
+    }
+);
--- js/main.js
+++ js/main.js
@@ -1,32 +1,48 @@
 define([
         'dojo/_base/declare',
-        // 'dojo/_base/lang',
         'dojo/request',
         'dijit/form/Button',
+        'dijit/MenuItem',
         'JBrowse/Plugin',
-        './View/Dialog/SnowLocateDialog'
+        './View/Dialog/SnowLocateDialog',
+        './View/Dialog/SnowMassTrackSettingDialog'
     ],
     function(
         declare,
-        // lang,
         dojoRequest,
         dijitButton,
+        dijitMenuItem,
         JBrowsePlugin,
-        SnowLocateDialog
+        SnowLocateDialog,
+        SnowMassTrackSettingDialog
     ){
         return declare( JBrowsePlugin,
             {
                 constructor: function( args )
                 {
+                    // console.log( "SnowPlugin plugin starting" );
                     var browser = args.browser;
+                    var _this = this;
                     this.browser = browser;
                     var locateButtonDomNode = this._generateLocateButton();
 
-                    // console.log( "SnowPlugin plugin starting" );
                     console.info('高通量多组学序列数据可视化浏览器 v1.0\nadmin@xuefeng.space\n指导老师: 钟坚成');
                     browser.afterMilestone('initView', function() {
                             var menuBar = browser.menuBar;
                             menuBar.appendChild(locateButtonDomNode);
+
+                            browser.addGlobalMenuItem(
+                                'file',
+                                new dijitMenuItem(
+                                    {
+                                        label: 'Set mass spectra track number',
+                                        iconClass: 'dijitIconConfigure',
+                                        onClick: function () {
+                                            _this._displayMassTrackSettingDialog(_this.browser);
+                                        }
+                                    }
+                                )
+                            );
                         }
                     );
                 },
@@ -37,7 +53,7 @@ define([
                     var locateButton = new dijitButton(
                         {
                             className :"locate-button",
-                            innerHTML:"Locate",
+                            innerHTML:"<span class=\"icon\"></span> Locate",
                             title: "Locate specific protein",
                             onClick : function(){
                                 _this._displayLocateDialog(_this.browser);
@@ -80,6 +96,27 @@ define([
                     );
 
                     locateDialog.show();
+                },
+
+                _displayMassTrackSettingDialog: function (browserObject)
+                {
+                    var _this = this;
+
+                    var massTrackSettingDialog = new SnowMassTrackSettingDialog(
+                        {
+                            browser: browserObject,
+                            setCallback: function (massTrackNumber) {
+                                console.info('massTrackNumber:', massTrackNumber);
+                                if(isNaN(massTrackNumber) || massTrackNumber < 0 || massTrackNumber > 100)
+                                {
+                                    return;
+                                }
+                                _this.browser.config.massSpectraTrackNum = massTrackNumber;
+                            }
+                        }
+                    );
+
+                    massTrackSettingDialog.show();
                 },
 
                 _queryProteinRegion: function (proteinName, finishCallback)
